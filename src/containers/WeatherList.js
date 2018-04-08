3bc6f10b8ef0fe5e2b14d8fd8a11bf392bae6e07
ccm36 <cameron.c.malloy@gmail.com>
2018-04-08 09:34:11 -0500

    Create WeatherChart component to display weather data

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import WeatherChart from '../components/WeatherChart'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -9,15 +9,13 @@ class WeatherList extends Component {
     const humidities = cityData.list.map(weather => weather.main.humidity)
     const pressures = cityData.list.map(weather => weather.main.pressure)
 
-    console.log(temps, humidities, pressures)
-
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
+          <WeatherChart data={temps} color="red" />
+          <WeatherChart data={pressures} color="blue" />
+          <WeatherChart data={humidities} color="green" />
         </td>
       </tr>
     )
