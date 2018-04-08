3e2af2d83c433ce8ff19fd16fe2453d9a584c2fa
ccm36 <cameron.c.malloy@gmail.com>
2018-04-07 19:14:27 -0500

    Begin axios request to fetch weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const { API_KEY } = require('../config')
+import { API_KEY } from '../config'
+import axios from 'axios'
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city, country = 'us') {
+  const url = `${ROOT_URL}&q=${city},${country}`
+  const request = axios.get(url)
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  }
+}
